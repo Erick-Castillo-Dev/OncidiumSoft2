6fdd2df9620ea12e871d4eff99ea860019718ba5
JuankaMendez <lox625@gmail.com>
2016-11-01 16:05:46 -0600

    Cambio de nombre

--- OncidiumSoft/OncidiumSoft/Formularios/FrmPrincipal.Designer.cs
+++ OncidiumSoft/OncidiumSoft/Formularios/FrmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OncidiumSoft
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FrmPrincipal";
+            this.Text = "FrmPrincipal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
