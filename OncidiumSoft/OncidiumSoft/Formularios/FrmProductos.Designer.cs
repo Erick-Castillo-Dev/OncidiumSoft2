85ea55c0a45f5f2c6f11c310b19669d235d7ca88
JuankaMendez <lox625@gmail.com>
2016-11-02 17:10:52 -0600

    Agregacion de elementos al Formulario.

--- OncidiumSoft/OncidiumSoft/Formularios/FrmProductos.Designer.cs
+++ OncidiumSoft/OncidiumSoft/Formularios/FrmProductos.Designer.cs
@@ -28,11 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgVProductos = new System.Windows.Forms.DataGridView();
+            this.lblProductos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgVProductos
+            // 
+            this.dgVProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVProductos.Location = new System.Drawing.Point(38, 127);
+            this.dgVProductos.Name = "dgVProductos";
+            this.dgVProductos.Size = new System.Drawing.Size(499, 265);
+            this.dgVProductos.TabIndex = 0;
+            // 
+            // lblProductos
+            // 
+            this.lblProductos.AutoSize = true;
+            this.lblProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductos.Location = new System.Drawing.Point(239, 45);
+            this.lblProductos.Name = "lblProductos";
+            this.lblProductos.Size = new System.Drawing.Size(395, 31);
+            this.lblProductos.TabIndex = 1;
+            this.lblProductos.Text = "Administración de Productos.";
+            // 
+            // FrmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 454);
+            this.Controls.Add(this.lblProductos);
+            this.Controls.Add(this.dgVProductos);
+            this.Name = "FrmProductos";
             this.Text = "FrmProductos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgVProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgVProductos;
+        private System.Windows.Forms.Label lblProductos;
     }
 }
