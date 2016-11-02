da107903ada5d435b1309d196b17e3f93314be36
MyFirstApp <pepe_chiquilin_@hotmail.com>
2016-11-01 21:34:07 -0600

    Creación del formulario ajuste de inventario

---
+++ OncidiumSoft/OncidiumSoft/Formularios/FrmAjusteInventario.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace OncidiumSoft.Formularios
+{
+    partial class FrmAjusteInventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblTipoAjuste = new System.Windows.Forms.Label();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblPrecioCliente = new System.Windows.Forms.Label();
+            this.txtPrecioCliente = new System.Windows.Forms.TextBox();
+            this.lblPrecioCosto = new System.Windows.Forms.Label();
+            this.txtPrecioCosto = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.cbProducto = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(183, 280);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 44;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(92, 280);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 43;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 236);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 21);
+            this.comboBox1.TabIndex = 42;
+            // 
+            // lblTipoAjuste
+            // 
+            this.lblTipoAjuste.AutoSize = true;
+            this.lblTipoAjuste.Location = new System.Drawing.Point(12, 239);
+            this.lblTipoAjuste.Name = "lblTipoAjuste";
+            this.lblTipoAjuste.Size = new System.Drawing.Size(75, 13);
+            this.lblTipoAjuste.TabIndex = 41;
+            this.lblTipoAjuste.Text = "Tipo de Ajuste";
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Location = new System.Drawing.Point(92, 193);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(167, 21);
+            this.cbEstado.TabIndex = 40;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(13, 196);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(40, 13);
+            this.lblEstado.TabIndex = 39;
+            this.lblEstado.Text = "Estado";
+            // 
+            // lblPrecioCliente
+            // 
+            this.lblPrecioCliente.AutoSize = true;
+            this.lblPrecioCliente.Location = new System.Drawing.Point(13, 154);
+            this.lblPrecioCliente.Name = "lblPrecioCliente";
+            this.lblPrecioCliente.Size = new System.Drawing.Size(72, 13);
+            this.lblPrecioCliente.TabIndex = 38;
+            this.lblPrecioCliente.Text = "Precio Cliente";
+            // 
+            // txtPrecioCliente
+            // 
+            this.txtPrecioCliente.Location = new System.Drawing.Point(92, 147);
+            this.txtPrecioCliente.Name = "txtPrecioCliente";
+            this.txtPrecioCliente.Size = new System.Drawing.Size(167, 20);
+            this.txtPrecioCliente.TabIndex = 37;
+            // 
+            // lblPrecioCosto
+            // 
+            this.lblPrecioCosto.AutoSize = true;
+            this.lblPrecioCosto.Location = new System.Drawing.Point(13, 111);
+            this.lblPrecioCosto.Name = "lblPrecioCosto";
+            this.lblPrecioCosto.Size = new System.Drawing.Size(67, 13);
+            this.lblPrecioCosto.TabIndex = 36;
+            this.lblPrecioCosto.Text = "Precio Costo";
+            // 
+            // txtPrecioCosto
+            // 
+            this.txtPrecioCosto.Location = new System.Drawing.Point(92, 104);
+            this.txtPrecioCosto.Name = "txtPrecioCosto";
+            this.txtPrecioCosto.Size = new System.Drawing.Size(167, 20);
+            this.txtPrecioCosto.TabIndex = 35;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(12, 69);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblCantidad.TabIndex = 34;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // cbProducto
+            // 
+            this.cbProducto.FormattingEnabled = true;
+            this.cbProducto.Location = new System.Drawing.Point(91, 20);
+            this.cbProducto.Name = "cbProducto";
+            this.cbProducto.Size = new System.Drawing.Size(167, 21);
+            this.cbProducto.TabIndex = 33;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(91, 62);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(167, 20);
+            this.txtCantidad.TabIndex = 32;
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(12, 28);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(50, 13);
+            this.lblProducto.TabIndex = 31;
+            this.lblProducto.Text = "Producto";
+            // 
+            // FrmAjusteInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 322);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblTipoAjuste);
+            this.Controls.Add(this.cbEstado);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblPrecioCliente);
+            this.Controls.Add(this.txtPrecioCliente);
+            this.Controls.Add(this.lblPrecioCosto);
+            this.Controls.Add(this.txtPrecioCosto);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.cbProducto);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblProducto);
+            this.Name = "FrmAjusteInventario";
+            this.Text = "FrmAjusteInventario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblTipoAjuste;
+        private System.Windows.Forms.ComboBox cbEstado;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblPrecioCliente;
+        private System.Windows.Forms.TextBox txtPrecioCliente;
+        private System.Windows.Forms.Label lblPrecioCosto;
+        private System.Windows.Forms.TextBox txtPrecioCosto;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.ComboBox cbProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblProducto;
+    }
+}
