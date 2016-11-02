490b0e02e4d4fd2678af16318623ca0462b9d90e
EdgarCoronaRD <edgarcoron101@gmail.com>
2016-11-02 11:53:39 -0600

    Agregar  compras y pedios

---
+++ OncidiumSoft/OncidiumSoft/Formularios/frmListPedidos.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace OncidiumSoft.Formularios
+{
+    partial class frmListPedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmListPedidos));
+            this.btnRealizar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DGVlistapedidos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVlistapedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRealizar
+            // 
+            this.btnRealizar.Image = ((System.Drawing.Image)(resources.GetObject("btnRealizar.Image")));
+            this.btnRealizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRealizar.Location = new System.Drawing.Point(56, 393);
+            this.btnRealizar.Name = "btnRealizar";
+            this.btnRealizar.Size = new System.Drawing.Size(157, 37);
+            this.btnRealizar.TabIndex = 31;
+            this.btnRealizar.Text = "Aceptar";
+            this.btnRealizar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(504, 393);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(157, 37);
+            this.btnCancelar.TabIndex = 29;
+            this.btnCancelar.Text = "Cancelar ";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(573, 20);
+            this.textBox1.TabIndex = 28;
+            this.textBox1.Text = "Buscar......";
+            // 
+            // DGVlistapedidos
+            // 
+            this.DGVlistapedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVlistapedidos.Location = new System.Drawing.Point(24, 93);
+            this.DGVlistapedidos.Name = "DGVlistapedidos";
+            this.DGVlistapedidos.Size = new System.Drawing.Size(859, 274);
+            this.DGVlistapedidos.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(726, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 37);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Menu Principal";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(283, 393);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(157, 41);
+            this.btnEliminar.TabIndex = 30;
+            this.btnEliminar.Text = "Actualizar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // frmListPedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 478);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRealizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DGVlistapedidos);
+            this.Name = "frmListPedidos";
+            this.Text = "frmListPedidos";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVlistapedidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRealizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView DGVlistapedidos;
+    }
+}
