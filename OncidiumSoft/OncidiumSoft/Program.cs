110bf424e415613bbad2881e6bf773236aed1e47
Erick Castillo <w-wwerick@hotmail.com>
2016-11-04 08:29:56 -0600

    Arreglo

--- OncidiumSoft/OncidiumSoft/Program.cs
+++ OncidiumSoft/OncidiumSoft/Program.cs
@@ -17,7 +17,7 @@ namespace OncidiumSoft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmContabilidad());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
