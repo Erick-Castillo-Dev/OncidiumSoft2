463efb466432c319c8b5b4cbe761231d21008d31
Erick Castillo Castro <w-wwerick@hotmail.com>
2016-11-25 07:56:40 -0600

    Formulario de cargar

---
+++ OncidiumSoft/OncidiumSoft/Formularios/FrmCargar.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace OncidiumSoft.Formularios
+{
+    partial class FrmCargar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCargar));
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.SuspendLayout();
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = ((WinFormAnimation.AnimationFunctions.Function)(resources.GetObject("circularProgressBar1.AnimationFunction")));
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.White;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(64, 36);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.White;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.circularProgressBar1.ProgressWidth = 25;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(243, 250);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 1;
+            this.circularProgressBar1.Text = "Cargando...";
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(0);
+            // 
+            // FrmCargar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 326);
+            this.Controls.Add(this.circularProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmCargar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FrmCargar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+    }
+}
---
+++ OncidiumSoft/OncidiumSoft/Formularios/FrmCargar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace OncidiumSoft.Formularios
+{
+    public partial class FrmCargar : Form
+    {
+        public FrmCargar()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Variables de control de carga
+        /// </summary>
+        public bool si = false;
+        int tiempo = 50;
+        public string s;
+        /// <summary>
+        /// Metodo que carga el formulario y ejecuta las intrucciones para cada caso dependiendo de las variables de control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmCargar_Load(object sender, EventArgs e)
+        {
+            if(si){
+                tiempo = 30;
+            }
+            this.BackColor = Color.ForestGreen;
+            this.TransparencyKey = BackColor;
+            circularProgressBar1.BackColor = BackColor;
+            circularProgressBar1.Value = 0;
+            circularProgressBar1.Minimum = 0;
+            circularProgressBar1.Maximum = 100;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(tiempo);
+                circularProgressBar1.Value = i;
+                circularProgressBar1.Update();
+            }
+            if(si){
+                this.Opacity = 0;
+                FrmPrincipal p = new FrmPrincipal();
+                p.idUsuario2.Text = s;
+                p.Show();
+            }
+            else
+            {
+                this.Opacity = 0;
+                new FrmLogin().Show();
+            }
+        }
+    }
+}
